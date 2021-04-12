193d3a1b7cb1f56f07393142560e61edf15609f0
bferguson3 <ferguson.ben86@gmail.com>
2021-04-12 10:11:35 -0400

    Some def updates

--- src/main.c
+++ src/main.c
@@ -2,18 +2,37 @@ typedef const unsigned char String[];
 typedef unsigned char u8;
 typedef unsigned int u16;
 
-const unsigned char str[] = "Hello World!\x00";
+#define FLASH (1<<7)
+#define BRIGHT (1<<6)
+#define PAPER(n) (n<<3)
+#define INK(n) n 
+#define VRAM_BASE 0x4000
+#define CRAM_BASE 0x5800
+
+String str = "Hello World!\x00";
 unsigned char b;
+u16 temp;
 
 u16 GetVRAMPos(u8 x, u8 y);
 
 void main()
 {
     inits();
-    volatile unsigned char* p;
-    p = (unsigned char*)(0x4000 + 3);
+    
+    volatile u8* p;
+    // Draw a line 
+    p = (u8*)(VRAM_BASE + 3);
     *p = 255;
+    // Draw a dotted line
+    temp = GetVRAMPos(15, 30);
+    p = (u8*)temp;
+    *p = 0b10101010;
+    // Set color to red on pink
+    p = (u8*)CRAM_BASE + 3;
+    *p = BRIGHT|PAPER(3)|INK(2);
+    
     while(1){ }
+    
     return;
 }
 
@@ -33,21 +52,30 @@ u16 GetVRAMPos(u8 x, u8 y) __naked
         ld	    b, 4 (ix)
         ld	    c, 5 (ix)
         
-      /*  
-        push	ix
-        ld	ix,#0
-        add	ix,sp
-    ;src/main.c:28: return x + y;
-        ld	c, 4 (ix)
-        ld	b, #0x00
-        ld	l, 5 (ix)
-        ld	h, #0x00
-        add	hl, bc
-    ;src/main.c:30: __endasm;
-    ;src/main.c:31: }
-        pop	ix
-        ret
-    */
+        ld      a, b 
+        and     #0b00000111
+        or      #0b01000000
+        ld      h, a 
+        ld      a, b 
+        rra 
+        rra 
+        rra 
+        and     #0b00011000
+        or      h 
+        ld      h, a 
+        ld      a, b 
+        rla 
+        rla 
+        and     #0b11100000 
+        ld      l, a 
+        ld      a, c 
+        rra     
+        rra     
+        rra     
+        and     #0b00011111
+        or      l 
+        ld      l, a 
+    
         pop	    ix
         ret
     __endasm;
